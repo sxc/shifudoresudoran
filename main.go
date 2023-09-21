67fe93424fbb216e7bdea9d970e7581092a83b16
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-21 15:12:40 +0800

    add modd and params

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, " + name + "!",
+	})
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.GET("/:name", IndexHandler)
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
