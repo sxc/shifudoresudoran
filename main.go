7a41e475e35d6b7fd612ce0e2b0d0ffe26248de1
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-21 15:57:34 +0800

    add XML render

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"encoding/xml"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func IndexHandler(c *gin.Context) {
-	name := c.Params.ByName("name")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, " + name + "!",
-	})
-}
-
 func main() {
 	r := gin.Default()
-	r.GET("/:name", IndexHandler)
+	r.GET("/", IndexHandler)
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(http.StatusOK, Person{
+		FirstName: "John",
+		LastName:  "Appleseed",
+	})
 }
