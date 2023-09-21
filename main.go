065516828146245c9762bb9bc14b2c006f3190f6
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-21 22:26:28 +0800

    recipe data model

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,4 +25,12 @@ func IndexHandler(c *gin.Context) {
 		FirstName: "John",
 		LastName:  "Appleseed",
 	})
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
