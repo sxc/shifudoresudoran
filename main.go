54fa9e7bb35c1002ac027f760a50eaae5869a96f
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-22 14:41:21 +0800

    update recipe endpoint

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	r := gin.Default()
 	r.POST("/recipes", NewRecipeHandler)
 	r.GET("/recipes", ListRecipesHandler)
+	r.PUT("/recipes/:id", UpdateRecipeHandler)
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -48,4 +49,25 @@ func NewRecipeHandler(c *gin.Context) {
 
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
 }
