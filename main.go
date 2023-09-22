e8ceacfb3aa880abe2f606291afd933c5cd90d98
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-22 21:35:57 +0800

    added openapi specs

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/sxc/shifudoresudoran.
+//
+// Schemes: http
+// Host: localhost:3000
+// BasePath: /
+// Version: 1.0.0
+// Contact : Jim
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
+
 package main
 
 import (
@@ -50,10 +67,61 @@ func NewRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+// swagger:operation GET /recipes recipes listRecipes
+
+// Returns list of recipes
+
+// ---
+
+// produces:
+
+// - application/json
+
+// responses:
+
+//     '200':
+
+//         description: Successful operation
+
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+
+// Update an existing recipe
+
+// ---
+
+// parameters:
+
+// - name: id
+
+//   in: path
+
+//   description: ID of the recipe
+
+//   required: true
+
+//   type: string
+
+// produces:
+
+// - application/json
+
+// responses:
+
+//     '200':
+
+//         description: Successful operation
+
+//     '400':
+
+//         description: Invalid input
+
+//     '404':
+
+// description: Invalid recipe ID
 func UpdateRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
 	var recipe Recipe
